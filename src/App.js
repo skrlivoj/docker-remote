7e5899ef36715b864f668a6f97c20fdc46e104d4
Vojtech Skrlik <vojtech.skrlik@email.com>
2024-07-25 11:01:00 +0200

    another app text change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React is awesome!
+          React is awesome
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders React is awesome! link', () => {
+test('renders React is awesome link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/React is awesome!/i);
+  const linkElement = screen.getByText(/React is awesome/i);
   expect(linkElement).toBeInTheDocument();
 });
